76813b12dff0c4c6e65f00454a59ddf9c3d57c60
meghna0122 <91670955+meghna0122@users.noreply.github.com>
2024-02-23 23:12:36 +0530

    Oops

---
+++ m_50_oops_interface.java
@@ -0,0 +1,24 @@
+public class m_50_oops_interface {
+  
+    //Interface is blueprint of class
+    // It is used to implement multiple inheritance 
+    // To achieve total abstraction..100% abstraction
+    //all methods in interface are public and abstract and without implementation
+    
+    public static void main(String args[]){  
+        Drawable d=new Rectangle();  
+        d.draw();  
+        System.out.println(Drawable.cube(3));  
+        
+    }}
+    interface Drawable{  
+        void draw();  
+        static int cube(int x){return x*x*x;}  
+        }  
+        class Rectangle implements Drawable{  
+        public void draw(){System.out.println("drawing rectangle");}  
+        }  
+          
+        
+    
+
