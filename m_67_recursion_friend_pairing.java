f0d95080d949e503aea795d4bd3c263aafa1519d
meghna0122 <91670955+meghna0122@users.noreply.github.com>
2024-03-03 09:17:54 +0530

    recursion

---
+++ m_67_recursion_friend_pairing.java
@@ -0,0 +1,17 @@
+public class m_67_recursion_friend_pairing {
+    public static int friendPair(int n){
+        if(n==1 || n==2){
+            return n;
+        }
+        int fn1=friendPair(n-1);
+        int fn2 =friendPair(n-2);
+        int pair= (n-1)* fn2;
+        int totalways=fn1+pair;
+        return totalways;
+
+    }
+public static void main(String[] args) {
+    System.out.println(friendPair(3));
+}
+    
+} 
